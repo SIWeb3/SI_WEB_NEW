f2ab48569b5dddbcb44da4f541bf73facd84e712
DindaAP-PC\Dinda AP <dindaayup012@gmail.com>
2018-05-23 00:06:57 +0700

    Rek ini format CI yang baru, untuk nama database kalian bikin nama databasenya "project_web" database masih pake punya e imel, but blm di relasikan. aku udah buat halaman utama doang. jadi cuman Home aja, blm ada isinya, cuman ws ada tampilane

--- application/controllers/gass.php
+++
@@ -1,7 +0,0 @@
-<?php
-class GolA extends CI_Controller{
-
-public function index (){
-echo "";
-}
-}
---
+++ application/views/footer.php
@@ -0,0 +1,21 @@
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Sistem Informasi Pengajuan Judul Tugas Akhir 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url ('assets/vendor/jquery/jquery.min.js') ?>"></script>
+    <script src="<?php echo base_url ('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url ('assets/vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
+
+    <!-- Custom JavaScript for this theme -->
+    <script src="<?php echo base_url ('assets/js/scrolling-nav.js') ?>"></script>
+
+  </body>
+
+</html>
