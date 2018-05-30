3823632901437d6b1f048b63626e38a3a9db466b
anggrika amelia y <anggrikaamelia238@gmail.com>
2018-05-30 12:07:39 +0700

    file isi kuota

--- application/views/kordinator/kuotadosen.php
+++ application/views/kordinator/kuotadosen.php
@@ -1,70 +1,67 @@
-<section id="about">
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Kuota Dosen</h3>
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+              <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fa fa-times"></i></button>
+          </div>
+        </div>
+        <div class="box-body">
+		<?php if(isset($error)){ ?>
+                    <div class="alert alert-danger alert-dismissible">
+                      <a herf="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                      <?php echo $error; ?>
+                    </div>
+              <?php } ?>
+			  <?php if(isset($success)){ ?>
+                    <div class="alert alert-info alert-dismissible">
+                      <a herf="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                      <?php echo $success; ?>
+                    </div>
+              <?php } ?>
+          <section id="about">
       <div class="container">
         <div class="row">
           <div class="col-lg-8 mx-auto">
-            <form action="<?php echo base_url('welcomemhs/inputjudul') ?>" method="post">
+            <form action="<?php echo base_url('kordinator/isikuota') ?>" method="post">
+              <div class="card">
               
 
               <div class="card-body p-4">
     
-                 <div class="form-group">
-                  <label for="nim">NIM</label>
-                  <input type="text" name="nim" class="form-control" required="">
-                </div>
 
                 <div class="form-group">
-                  <label for="prodi">Program Studi</label>
-                  <select name="prodi" class="form-control" required="">
-                    <option value="mif">Manajemen Informatika</option>
-                    <option value="tkk">Teknik Komputer</option>
-                    <option value="tif">Teknik Informatika</option>
-                  </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="golongan">Golongan</label>
-                  <select name="golongan" class="form-control" required="">
-                    <option value="a">A</option>
-                    <option value="b">B</option>
-                    <option value="c">C</option>
-                     <option value="d">D</option>
-                  </select>
-              </div>
-              
-                <div class="form-group">
-                  <label for="judul">Usulan Judul Tugas Akhir</label>
-                  <input type="text" name="judul" class="form-control" required="">
-                </div>
-
-               <div class="form-group">
-                  <label for="pengerjaan">Individu/Kelompok</label>
-                  <select name="pengerjaan" class="form-control" required="">
-                    <option value="individu">Individu</option>
-                    <option value="kelompok">Kelompok</option>
-                  </select>
-                </div>
-
-                <div class="form-group">
-                  <label for="deskripsi">Deskripsi</label>
-                  <textarea name="deskripsi" class="form-control" required=""></textarea> 
-                </div>
-
-                <div class="form-group">
-                  <label for="dosen_pembimbing">Usulan Dosen Pembimbing</label>
-                  <select name="dosen_pembimbing" class="form-control" required="">
-                    <option>--Pilih Usulan Dosen Pembimbing--</option>
+                  <label for="dosen_pembimbing">Nama Dosen</label>
+                  <select name="nama_dosen" class="form-control" required="">
                     <?php foreach ($data_dosen as $row){ ?>
                     <option value="<?php echo $row->nip;?>"><?php echo $row->nama_dosen;?></option>
                     <?php }?>
                   </select>
                 </div>
 
-
-
+                
+              
+                <div class="form-group">
+                  <label for="kuota dosen">Kuota Dosen</label>
+                  <input type="number" name="kuota" class="form-control" required="">
+                </div>
+				
               </div>
 
               <div class="card-footer text-right">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" name="kirim" class="btn btn-primary">Submit</button>
               </div>
             </div>
           </form>
@@ -72,3 +69,16 @@
         </div>
       </div>
     </section>
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer">
+          Footer
+        </div>
+        <!-- /.box-footer-->
+      </div>
+      <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
