f09e949f0e005456aca29b875a0ad2a506a5c02b
DindaAP-PC\Dinda AP <dindaayup012@gmail.com>
2018-06-24 17:42:22 +0700

    home yang error udah bener

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,7 +6,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('header');
-		$this->load->view('home');
+		$this->load->view('awal');
 		$this->load->view('footer');
 	}
 
---
+++ application/views/awal.php
@@ -0,0 +1,17 @@
+   <header class="bg-primary text-white">
+      <div class="container text-center">
+        <h1>Selamat Datang </h1>
+        <p class="lead">Di Sistem Informasi Pengajuan Judul Tugas Akhir</p>
+      </div>
+    </header>
+
+    <section id="home">
+<div class="container">
+        <div class="row">
+          <div class="col-lg-12 mx-auto">
+            
+
+        </div>
+      </div>
+    </section>
+
