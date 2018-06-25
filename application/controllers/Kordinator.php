1e00ba91309493b5a02d034b5ce2afd1595249ae
BagusSE\asus <sgaultts.bs>
2018-06-25 17:13:40 +0700

    edit link judul dosen

--- application/controllers/Kordinator.php
+++ application/controllers/Kordinator.php
@@ -53,7 +53,7 @@ class Kordinator extends CI_Controller {
 
 	public function inputjuduldosen(){
 		$data = array(
-			'uniqid'=>'juduldosen',
+			'uniqid'=>'cobajuduldosen',
 		);
 
 		$this->load->view('kordinator/content',$data);
